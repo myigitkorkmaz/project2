8a8e8ed2fbc893ef15ebfc10145fa974d2c67a3c
Mehmet Yigit Korkmaz <korkmazm1@newpaltz.edu>
2026-03-06 13:31:28 -0500

    push brach test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("pr 2 git trial");
+        System.out.println("the change made to test push first in the branch");
     }
 }
